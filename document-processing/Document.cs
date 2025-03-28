1976cb8434e757d317aebf8eafd9f564cf752582
sushinectar <lucianoxjktop@gmail.com>
2025-03-28 00:24:02 -0300

    adding constructor

--- document-processing/Document.cs
+++ document-processing/Document.cs
@@ -1,5 +1,13 @@
-public class Document {
+public class Document 
+{
   public string Title { get; set; }
   public string Author { get; set; }
   public DateTime Date { get; set; }
+
+  public Document (string title, string author, DateTime date) 
+  {
+    Title = title;
+    Author = author;
+    Date = date;
+  }
 }
