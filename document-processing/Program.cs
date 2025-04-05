9fc34a92108bb6ed334a52eedb22bacc92466d49
sushinectar <lucianoxjktop@gmail.com>
2025-04-05 17:50:48 -0300

    changing structure

--- document-processing/Program.cs
+++ document-processing/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Classe base
+public abstract class Documento
+{
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public DateTime DataCriacao { get; set; }
+
+    public Documento(string titulo, string autor)
+    {
+        Titulo = titulo;
+        Autor = autor;
+        DataCriacao = DateTime.Now;
+    }
+
+    public virtual void Imprimir()
+    {
+        Console.WriteLine($"Título: {Titulo}");
+        Console.WriteLine($"Autor: {Autor}");
+        Console.WriteLine($"Data de Criação: {DataCriacao}");
+    }
+
+    public virtual string ConteudoFormatado()
+    {
+        return $"Título: {Titulo}\nAutor: {Autor}";
+    }
+}
