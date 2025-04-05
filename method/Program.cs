cf5bb93e25e23673c5e3c47af3298cf18c451726
sushinectar <lucianoxjktop@gmail.com>
2025-04-05 18:02:16 -0300

    creating the class base

--- method/Program.cs
+++ method/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Classe base
+public abstract class Notificacao
+{
+    public string Destinatario { get; set; }
+    public string Mensagem { get; set; }
+    public DateTime DataEnvio { get; private set; }
+
+    public Notificacao(string destinatario, string mensagem)
+    {
+        Destinatario = destinatario;
+        Mensagem = mensagem;
+        DataEnvio = DateTime.Now;
+    }
+
+    public virtual void Enviar()
+    {
+        Console.WriteLine("Enviando notificação...");
+    }
+
+    public virtual string FormatarMensagem()
+    {
+        return $"Para: {Destinatario}\nMensagem: {Mensagem}";
+    }
+}
