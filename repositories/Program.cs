f1cba467d9494fee643a52ce56dfee7042b2df61
sushinectar <lucianoxjktop@gmail.com>
2025-04-06 14:39:55 -0300

    creating interface with CRUD

--- repositories/Program.cs
+++ repositories/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Interface com métodos CRUD
+public interface IRepositorio<T>
+{
+    void Create(T entidade);
+    T Read(int id);
+    void Update(int id, T entidade);
+    void Delete(int id);
+    List<T> ListarTodos();
+}
